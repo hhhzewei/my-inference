8fdd7d480fdec63740a9367a793fe86eb70b86cc
hhhzewei <761890156@qq.com>
2026-04-26 17:24:19 +0800

    遗漏

--- src/graph/graph.cpp
+++ src/graph/graph.cpp
@@ -12,6 +12,7 @@
 #include "memory/memory_planning.h"
 #include "memory/memory_allocator/memory_allocator.h"
 #include "memory/memory_allocator/memory_allocator_util.h"
+#include "optimize/optimizer.h"
 #include "optimize/optimizer_util.h"
 #include "util/onnx_util.h"
 
@@ -24,22 +25,22 @@ std::unique_ptr<Graph> Graph::make(const std::string &onnx_path) {
 }
 
 void Graph::optimize() {
-    for (Optimizer *optimizer: CommonOptimizers) {
-        (*optimizer)(this);
+    for (PassType pass: optimizer_passes) {
+        if (Optimizer *optimizer = getOptimizer(pass)) {
+            (*optimizer)(this);
+        }
     }
 }
 
 void Graph::prepare() {
     topoSort();
     planTensorMemory();
-    planMetaMemory();
 }
 
 void Graph::preRun() {
     memory_allocator_ = getMemoryAllocator(backend_);
     // allocate memory
     tensor_memory_pointer_ = static_cast<uint8_t *>(memory_allocator_->allocate(tensor_memory_size_));
-    meta_memory_pointer_ = static_cast<uint8_t *>(memory_allocator_->allocate(meta_memory_size_));
     // kernel sequence
     kernel_sequence_.reserve(topo_op_sequence_.size());
     for (const auto op: topo_op_sequence_) {
@@ -49,18 +50,11 @@ void Graph::preRun() {
 
         for (int i = 0; i < op->numInput(); ++i) {
             const auto input = op->input(i);
-            kernel_param.inputs.emplace_back(
-                tensor_memory_pointer_ + input->memoryInfo()->offset(),
-                reinterpret_cast<int64_t *>(meta_memory_pointer_ + input->shapeOffset()),
-                reinterpret_cast<int64_t *>(meta_memory_pointer_ + op->inputStridesOffset(i)));
+            kernel_param.inputs.emplace_back(tensor_memory_pointer_ + input->memoryInfo()->offset());
         }
         for (int i = 0; i < op->numOutput(); ++i) {
             const auto output = op->output(i);
-            kernel_param.outputs.emplace_back(
-                tensor_memory_pointer_ + output->memoryInfo()->offset(),
-                reinterpret_cast<int64_t *>(meta_memory_pointer_ + output->shapeOffset()),
-                reinterpret_cast<int64_t *>(meta_memory_pointer_ + op->outputStridesOffset(i))
-            );
+            kernel_param.outputs.emplace_back(tensor_memory_pointer_ + output->memoryInfo()->offset());
         }
         kernel_sequence_.emplace_back(getOpKernel(op, backend_), std::move(kernel_param));
     }
@@ -70,33 +64,6 @@ void Graph::preRun() {
         auto &memory_info = constant_tensor->memoryInfo();
         memory_allocator_->memCpy(memory_info->offset() + tensor_memory_pointer_, constant_tensor->data(),
                                   memory_info->size_value());
-    }
-    // prepare meta data
-    for (const auto op: topo_op_sequence_) {
-        // inputs strides
-        for (int i = 0; i < op->numInput(); ++i) {
-            auto p = reinterpret_cast<int64_t *>(meta_memory_pointer_ + op->inputStridesOffset(i));
-            for (auto &stride: op->inputStrides(i)) {
-                int64_t stride_value = stride.value();
-                memory_allocator_->memCpy(p++, &stride_value, sizeof(int64_t));
-            }
-        }
-        // outputs strides
-        for (int i = 0; i < op->numOutput(); ++i) {
-            auto p = reinterpret_cast<int64_t *>(meta_memory_pointer_ + op->outputStridesOffset(i));
-            for (auto &stride: op->outputStrides(i)) {
-                int64_t stride_value = stride.value();
-                memory_allocator_->memCpy(p++, &stride_value, sizeof(int64_t));
-            }
-        }
-    }
-    // tensor shape
-    for (const auto &[id,tensor]: tensor_repository_) {
-        auto p = reinterpret_cast<int64_t *>(meta_memory_pointer_ + tensor->shapeOffset());
-        for (auto &dim: tensor->shape()) {
-            int64_t dim_value = dim.value();
-            memory_allocator_->memCpy(p++, &dim_value, sizeof(int64_t));
-        }
     }
 }
 
@@ -109,8 +76,17 @@ bool Graph::run(const std::vector<void *> &inputs, const std::vector<void *> &ou
         memory_allocator_->memCpy(memory_info->offset() + tensor_memory_pointer_, inputs[i],
                                   memory_info->size_value());
     }
+    std::map<OpType, size_t> time_map;
     for (auto &[kernel,param]: kernel_sequence_) {
+        auto start = std::chrono::steady_clock::now();
         (*kernel)(param);
+        auto end = std::chrono::steady_clock::now();
+            const auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+        if (auto it = time_map.find(kernel->op()->type()); it != time_map.end()) {
+            it->second += time;
+        } else {
+            time_map.emplace(kernel->op()->type(),time);
+        }
     }
     // load output
     for (int i = 0; i < sinkOp_->numInput(); ++i) {
@@ -123,15 +99,14 @@ bool Graph::run(const std::vector<void *> &inputs, const std::vector<void *> &ou
 
 void Graph::postRun() const {
     memory_allocator_->deallocate(tensor_memory_pointer_);
-    memory_allocator_->deallocate(meta_memory_pointer_);
 }
 
 void Graph::init(const std::string &onnx_path) {
     loadOnnx(onnx_path);
-    inferDataTypeAndShape();
-    for (const auto &[id,tensor]: tensor_repository_) {
-        tensor->initMemSize();
+    for (auto &[id,op]: op_repository_) {
+        propagateAttribute(op.get());
     }
+    inferDataTypeAndShape();
 }
 
 void Graph::loadOnnx(const std::string &onnx_path) {
@@ -151,23 +126,6 @@ void Graph::loadOnnx(const std::string &onnx_path) {
     loadTensor<TensorType::INTERNAL>(graph.value_info(), global_tensor_map);
     // 解析输出
     loadTensor<TensorType::OUTPUT>(graph.output(), global_tensor_map);
-    // relate
-    for (auto &node_proto: graph.node()) {
-        OpNode *op = global_op_map[node_proto.name()];
-        std::vector<TensorNode *> inputs;
-        inputs.reserve(node_proto.input_size());
-        for (const auto &input_name: node_proto.input()) {
-            inputs.emplace_back(global_tensor_map[input_name]);
-        }
-        std::vector<TensorNode *> outputs;
-        outputs.reserve(node_proto.output_size());
-        for (auto &output_name: node_proto.output()) {
-            outputs.emplace_back(global_tensor_map[output_name]);
-        }
-        op->init(std::move(inputs), std::move(outputs));
-        // 补全默认属性
-        propagateAttribute(op);
-    }
 }
 
 void Graph::loadOp(const google::protobuf::RepeatedPtrField<onnx::NodeProto> &node_list,
@@ -177,44 +135,48 @@ void Graph::loadOp(const google::protobuf::RepeatedPtrField<onnx::NodeProto> &no
         const std::string &name = node.name();
         const OpType &type = getOpType(node.op_type());
         if (type == OpType::Constant) {
-            OpNode *op = createOp(name, type, {}, global_op_map);
-            createTensor(op, 0, node.attribute(0).t(), global_tensor_map);
+            auto constant = createTensor(node.attribute(0).t(), global_tensor_map);
+            createOp(name, type, {}, {constant}, {}, global_op_map);
         } else {
             std::map<AttributeKey, AttributeValue> attribute_map = loadAttribute(node.attribute());
-            OpNode *op = createOp(name, type, attribute_map, global_op_map);
-            for (int i = 0; i < node.output_size(); ++i) {
-                createTensor(node.output(i), op, i, global_tensor_map);
+            std::vector<TensorNode *> inputs;
+            inputs.reserve(node.input_size());
+            std::vector<TensorNode *> outputs;
+            outputs.reserve(node.output_size());
+            for (int i = 0; i < node.input_size(); ++i) {
+                auto input = createTensor(node.input(i), global_tensor_map);
+                inputs.emplace_back(input);
             }
+            for (int i = 0; i < node.output_size(); ++i) {
+                auto output = createTensor(node.output(i), global_tensor_map);
+                outputs.emplace_back(output);
+            }
+            createOp(name, type, std::move(inputs), std::move(outputs),
+                     std::move(attribute_map), global_op_map);
         }
     }
 }
 
-OpNode *Graph::createOp(const std::string &name, OpType type,
-                        const std::map<AttributeKey, AttributeValue> &attribute_map,
-                        std::map<std::string, OpNode *> &global_op_map) {
-    if (const auto it = global_op_map.find(name); it != global_op_map.end()) {
+TensorNode *Graph::createTensor(const std::string &name, std::map<std::string, TensorNode *> &global_tensor_map) {
+    if (const auto it = global_tensor_map.find(name); it != global_tensor_map.end()) {
         return it->second;
     }
-    OpId id = op_id_generator_.next();
-    auto [it,success] = op_repository_.emplace(
-        id, std::make_unique<OpNode>(id, name, type, attribute_map));
-    OpNode *raw_p = it->second.get();
-    global_op_map.emplace(name, raw_p);
-    if (type == OpType::Constant) {
-        constant_nodes_.emplace_back(raw_p);
-    }
+    TensorId id = tensor_id_generator_.next();
+    const auto [it, isSuccess] = tensor_repository_.emplace(id,
+                                                            std::make_unique<TensorNode>(id, name));
+    TensorNode *raw_p = it->second.get();
+    global_tensor_map.emplace(name, raw_p);
     return raw_p;
 }
 
-TensorNode *Graph::createTensor(OpNode *producer, const int output_idx, const onnx::TensorProto &tensor_proto,
+TensorNode *Graph::createTensor(const onnx::TensorProto &tensor_proto,
                                 std::map<std::string, TensorNode *> &global_tensor_map) {
     const std::string &name = tensor_proto.name();
-    const auto tensor = createTensor(name, producer, output_idx, global_tensor_map);
+    const auto tensor = createTensor(name, global_tensor_map);
     const std::vector<TensorDim> shape(tensor_proto.dims().begin(), tensor_proto.dims().end());
     const DataType data_type = getDataType(tensor_proto.data_type());
     tensor->init(data_type, shape);
     tensor->initData(tensor_proto.raw_data());
-    producer->init({}, {tensor});
     return tensor;
 }
 
@@ -223,24 +185,9 @@ void Graph::loadTensor(const google::protobuf::RepeatedPtrField<onnx::TensorProt
                        std::map<std::string, OpNode *> &global_op_map) {
     for (const onnx::TensorProto &tensor_proto: tensor_list) {
         const std::string &name = tensor_proto.name();
-        const auto producer = createOp("__" + name + "_PRODUCER__", OpType::Constant, {}, global_op_map);
-        createTensor(producer, 0, tensor_proto, global_tensor_map);
+        auto constant = createTensor(tensor_proto, global_tensor_map);
+        createOp("__" + name + "_PRODUCER__", OpType::Constant, {}, {constant}, {}, global_op_map);
     }
-}
-
-TensorNode *Graph::createTensor(const std::string &name, OpNode *producer, int output_idx,
-                                std::map<std::string, TensorNode *> &global_tensor_map) {
-    if (const auto it = global_tensor_map.find(name); it != global_tensor_map.end()) {
-        return it->second;
-    }
-    TensorId id = tensor_id_generator_.next();
-    const auto [it, isSuccess] = tensor_repository_.emplace(id,
-                                                            std::make_unique<TensorNode>(
-                                                                id, name, producer,
-                                                                output_idx));
-    TensorNode *raw_p = it->second.get();
-    global_tensor_map.emplace(name, raw_p);
-    return raw_p;
 }
 
 void Graph::inferDataTypeAndShape() const {
@@ -274,7 +221,7 @@ std::map<OpNode::Id, size_t> Graph::opOutDegrees() const {
 
 void Graph::topoSort() {
     topo_op_sequence_.reserve(op_repository_.size());
-    int topoIdx = 0;
+    int64_t topoIdx = 0;
     auto op_func = [&](OpNode *op) {
         if (op->type() == OpType::Constant) {
             // 常量节点不参与拓扑排序
@@ -322,41 +269,18 @@ void Graph::planTensorMemory() {
             << "%" << std::endl;
 }
 
-void Graph::planMetaMemory() {
-    meta_memory_size_ = 0;
-    for (auto &[id,op]: op_repository_) {
-        if (const auto op_type = op->type(); isVirtual(op_type)) {
-            continue;
-        }
-        // inputs strides
-        std::vector<uint64_t> inputs_strides_offset;
-        inputs_strides_offset.reserve(op->numInput());
-        for (auto &strides: op->inputsStrides()) {
-            inputs_strides_offset.emplace_back(meta_memory_size_);
-            meta_memory_size_ += strides.size() * sizeof(int64_t);
-        }
-        // outputs strides
-        std::vector<uint64_t> outputs_strides_offset;
-        outputs_strides_offset.reserve(op->numOutput());
-        for (auto &strides: op->outputsStrides()) {
-            outputs_strides_offset.emplace_back(meta_memory_size_);
-            meta_memory_size_ += strides.size() * sizeof(int64_t);
-        }
-        op->setStridesOffset(std::move(inputs_strides_offset), std::move(outputs_strides_offset));
-    }
-    // tensor shape
-    for (auto &[id,tensor]: tensor_repository_) {
-        tensor->setShapeOffset(meta_memory_size_);
-        meta_memory_size_ += tensor->numDim() * sizeof(int64_t);
-    }
-    std::cout << "Meta memory size: " << meta_memory_size_ << "B" << std::endl;
+
+TensorNode *Graph::createTensor(DataType data_type, std::vector<TensorDim> shape, void *raw_data) {
+    TensorId id = tensor_id_generator_.next();
+    const auto [it, isSuccess] = tensor_repository_.emplace(
+        id, std::make_unique<TensorNode>(id, std::to_string(id), data_type, std::move(shape),
+                                         raw_data));
+    return it->second.get();
 }
 
-
 TensorNode *Graph::createConstant(const DataType data_type, std::vector<TensorDim> shape, void *raw_data) {
-    OpNode *producer = createOp(OpType::Constant);
-    TensorNode *tensor = createTensor(producer, 0, data_type, std::move(shape), raw_data);
-    producer->init({}, {tensor});
+    TensorNode *tensor = createTensor(data_type, std::move(shape), raw_data);
+    createOp(OpType::Constant, {}, {tensor}, {});
     return tensor;
 }
 
@@ -369,26 +293,9 @@ OpNode *Graph::createOp(OpType type, std::vector<TensorNode *> inputs, std::vect
     const auto &op = it->second.get();
     if (op->type() == OpType::Constant) {
         constant_nodes_.emplace_back(op);
-    } else {
-        initStrides(op);
     }
+    initStrides(op);
     return op;
-}
-
-TensorNode *Graph::createTensor(OpNode *producer, const int output_idx, const DataType data_type,
-                                std::vector<TensorDim> shape,
-                                void *raw_data) {
-    return createTensor("__OP_" + std::to_string(producer->id()) + "_OUTPUT_",
-                        producer, output_idx, data_type, std::move(shape), raw_data);
-}
-
-TensorNode *Graph::createTensor(std::string name, OpNode *producer, int output_idx, DataType data_type,
-                                std::vector<TensorDim> shape, void *raw_data) {
-    TensorId id = tensor_id_generator_.next();
-    const auto [it, isSuccess] = tensor_repository_.emplace(
-        id, std::make_unique<TensorNode>(id, std::move(name), producer, output_idx, data_type, std::move(shape),
-                                         raw_data));
-    return it->second.get();
 }
 
 void Graph::shrinkOp(const std::set<OpId> &aliveIds) {
